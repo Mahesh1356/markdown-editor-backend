eebf4463203ca29a423aed6b4cb962e63e73de9c
Mahesh1356 <maheshtma13@gmail.com>
2024-07-10 22:18:27 +0530

    feat(app): md to html conversion route added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { marked } from './utils/markdown';
+import { Response } from 'express';
+import * as stream from 'stream';
+class MdContentDto {
+  content: string;
+}
 
-@Controller()
+@Controller('md')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('to-html')
+  convertMdToHtml(@Body() body: MdContentDto, @Res() res: Response) {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Disposition', 'attachment; filename="page.html"');
+    const readable = new stream.Readable();
+    readable._read = () => {};
+    readable.push(marked.parse(body.content).toString());
+    readable.push(null);
+    readable.pipe(res);
   }
 }
